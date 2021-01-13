9020bd452abcd8cd2068139d861faf83522dfaa6
Th3Wall <mandellidavide.95@gmail.com>
2021-01-13 02:10:05 +0100

    Header added and styled

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Fragment } from 'react';
 import { Homepage } from './pages/Homepage/Homepage';
 import { Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/Shop/Shop';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <Fragment>
+        <Header />
         <Switch>
           <Route exact path="/" component={Homepage} />
           <Route path="/shop" component={ShopPage} />
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+import './header.scss';
+import { Link } from 'react-router-dom';
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+
+function Header() {
+    return (
+        <div className='header'>
+            <Link className='logo-container' to="/">
+                <Logo className='logo' />
+            </Link>
+            <div className="options">
+                <Link className='option' to='shop'>
+                    SHOP
+                </Link>
+                <Link className='option' to='shop'>
+                    CONTACTS
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default Header
