af175581fa534deab55c269b281119edf939687a
Th3Wall <mandellidavide.95@gmail.com>
2021-01-21 03:13:45 +0100

    Revert "Removed redux-logger from production build"
    This reverts commit e211b7eeea5248c1db917e56dd22ebc8f2cdb1ed.

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,12 +4,7 @@ import rootReducer from './rootReducer';
 import { persistStore } from 'redux-persist';
 
 
-const middlewares = [];
-
-if (process.env.NODE_ENV === 'development') {
-    middlewares.push(logger);
-}
-
+const middlewares = [logger];
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 export const persistor = persistStore(store);
 
