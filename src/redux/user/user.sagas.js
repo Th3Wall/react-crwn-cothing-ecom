240dcdcb76c7622cd10027cd908735253aab87f5
Th3Wall <mandellidavide.95@gmail.com>
2021-01-29 02:32:36 +0100

    fixed error inside userSagas call

--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -63,6 +63,6 @@ export function* userSagas() {
     yield all([
         call(onGoogleSignInStart),
         call(onEmailSignInStart),
-        call(isUserAuthenticated)
+        call(onCheckUserSession)
     ]);
 }
