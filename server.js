cb5169fcb8de9b7199233f5d887d401b5ab80d69
Th3Wall <mandellidavide.95@gmail.com>
2021-02-11 01:35:37 +0100

    moved compression in production phase

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const path = require("path");
 const compression = require('compression');
+const enforce = require('express-sslify');
 
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
@@ -11,13 +12,13 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(cors());
 
 if (process.env.NODE_ENV === "production") {
+    app.use(compression());
+    app.use(enforce.HTTPS({ trustProtoHeader: true }));
     app.use(express.static(path.join(__dirname, "client/build")));
 
     app.get("*", function (req, res) {
@@ -29,6 +30,10 @@ app.listen(port, (error) => {
     if (error) throw error;
     console.log("Server running on port " + port);
 });
+
+app.get('/service-worker.js', (res, req) => {
+    res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
+})
 
 app.post("/payment", (req, res) => {
     const body = {
